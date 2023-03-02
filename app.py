abaf51a9cd5233d859b5f8b474e175d7522ebe27
2cheinz <cheinzwebdev@gmail.com>
2023-03-01 20:27:36 -0600

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,18 +6,10 @@ app = Flask(__name__)
 def home():
     if request.method == 'POST':
         number = request.form['number']
-        with open('data.txt', 'a') as f:
-            f.write(number + '\n')
-        return f'Thank you for submitting {number}!'
+        return render_template('index.html', number=number)
     else:
-        return '''
-            <form method="post">
-              <label for="number">Number:</label>
-              <input type="number" name="number" id="number" min="1" max="100" required>
-              <br><br>
-              <button type="submit">Submit</button>
-            </form>
-        '''
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
