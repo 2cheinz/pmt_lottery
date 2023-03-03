dabf9331336a8bb2e214af039a906478ede2f1c1
2cheinz <cheinzwebdev@gmail.com>
2023-03-02 20:44:21 -0600

    edited html py and sql db

--- process_form.py
+++ process_form.py
@@ -1,1 +1,28 @@
+from flask import Flask, request
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/submit_guess', methods=['POST'])
+def submit_guess():
+    guess = request.form['guess']
+
+    # Connect to the MySQL database
+    cnx = mysql.connector.connect(user='sqluser', password='password',
+                                  host='localhost', database='guesses')
+    cursor = cnx.cursor()
+
+    # Insert the guess into the database
+    query = "INSERT INTO guesses (guess) VALUES (%s)"
+    cursor.execute(query, (guess,))
+    cnx.commit()
+
+    # Close the database connection
+    cursor.close()
+    cnx.close()
+
+    return "Your guess has been recorded!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
