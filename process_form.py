594b9d87c1d761cbff037cbcca8c122ae3bfb861
Casey <87348212+2cheinz@users.noreply.github.com>
2023-03-04 08:09:00 -0600

    connect database

--- process_form.py
+++ process_form.py
@@ -1,28 +1,57 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect
 import mysql.connector
 
 app = Flask(__name__)
 
-@app.route('/submit_guess', methods=['POST'])
-def submit_guess():
-    guess = request.form['guess']
+#define a route to the index.html file
+@app.route('/', methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        guess = request.form["guess"]
 
-    # Connect to the MySQL database
-    cnx = mysql.connector.connect(user='sqluser', password='password',
-                                  host='localhost', database='guesses')
-    cursor = cnx.cursor()
+        # Connect to the MySQL database
+        cnx = mysql.connector.connect(user='sqluser', password='password',
+                                    host='localhost', database='guesses')
+        cursor = cnx.cursor()
 
-    # Insert the guess into the database
-    query = "INSERT INTO guesses (guess) VALUES (%s)"
-    cursor.execute(query, (guess,))
-    cnx.commit()
+        # Insert the guess into the database
+        query = "INSERT INTO guesses (guess) VALUES (%s)"
+        cursor.execute(query, (guess,))
+        cnx.commit()
 
-    # Close the database connection
-    cursor.close()
-    cnx.close()
+        # Close the database connection
+        cursor.close()
+        cnx.close()
 
-    return "Your guess has been recorded!"
+        print(f"Your guess is {guess}")
+
+    return render_template('index.html')
+
+
+# @app.route('/submit_guess', methods=['POST'])
+# def submit_guess():
+#     if request.methods == 'POST':
+#         guess = request.form['guess']
+
+#     # Connect to the MySQL database
+#     cnx = mysql.connector.connect(user='sqluser', password='password',
+#                                   host='localhost', database='guesses')
+#     cursor = cnx.cursor()
+
+#     # Insert the guess into the database
+#     query = "INSERT INTO guesses (guess) VALUES (%s)"
+#     cursor.execute(query, (guess,))
+#     cnx.commit()
+
+#     # Close the database connection
+#     cursor.close()
+#     cnx.close()
+
+#     print("Your guess has been recorded")
+    
+#     # redirect the user back to index.html
+#     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5500)
 
